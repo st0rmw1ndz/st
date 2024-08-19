6d23a6bee0ebe708572cc034bce417b2eaeb84f8
frosty <passedgoandgot200@gmail.com>
2024-08-19 22:52:05 +0000

    patch: font2

--- config.def.h
+++ config.def.h
@@ -6,6 +6,12 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+	"Terminus:pixelsize=12",
+	"Symbols Nerd Font:pixelsize=11:antialias=true:autohint=true",
+};
+
 static int borderpx = 2;
 
 /*
